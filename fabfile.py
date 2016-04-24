91868775638abb3008ef7d95e3296bbe30d77fac
jxskiss <jxskiss@126.com>
2016-04-24 19:24:55 +0800

    Add jupyter notebook auto build support.

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,13 @@
 
 from fabric.api import *
 from fabric.context_managers import *
+from os import listdir
+from os.path import join, abspath, dirname, isfile
+from os.path import basename, splitext, getmtime
 from datetime import date
+
+_root = dirname(abspath(__file__))
+_post = join(_root, 'content', 'post')
 
 
 def _commit(msg):
@@ -17,7 +23,25 @@ def _push():
           'https://github.com/jxskiss/hshsh.me.git gh-pages')
 
 
-def build():
+def nbconvert():
+    nbdir = join(_root, 'data', 'ipynbs')
+    nbfiles = filter(isfile, (join(nbdir, fn) for fn in listdir(nbdir)))
+    count = 0
+    with settings(warn_only=True):
+        for fn in nbfiles:
+            mdfile = join(_post, splitext(basename(fn))[0] + '.md')
+            if getmtime(mdfile) >= getmtime(fn):
+                continue
+            local('jupyter nbconvert --stdout --to html --template basic '
+                  '{} > {}'.format(fn, join(
+                    _post, splitext(basename(fn))[0] + '.md')))
+            count += 1
+        print('nbconvert: {} new notebooks converted.'.format(count))
+
+
+def build(ipynb='convert'):
+    if ipynb == 'convert':
+        nbconvert()
     with settings(warn_only=True):
         local('hugo')
 
